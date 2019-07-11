fd1b7ff5c4668c7102483009677942fa6368c490
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2019-07-11 13:21:25 +0100

    Create systemator.rb

---
+++ systemator.rb
@@ -0,0 +1,12 @@
+class Vapor < Formula
+  homepage "https://github.com/Einstore/Systemator"
+  version "0.0.3"
+  url "https://github.com/Einstore/Systemator/releases/download/0.0.3/macOS.tar.gz"
+  sha256 "962d94f1aca27be8894f086e15079250340a56c40cb8dc28a28590ebde7d0e43"
+
+  depends_on "libssh2"
+
+  def install
+    bin.install "systemator"
+  end
+end
